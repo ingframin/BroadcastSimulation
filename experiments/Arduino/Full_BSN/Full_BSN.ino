68c17bf582e682b1d01905856daf7b67049d83b3
Franco <framin2@gmail.com>
2020-01-14 17:38:55 +0100

    added BSN experiment

--- experiments/Arduino/Full_BSN/Full_BSN.ino
+++ experiments/Arduino/Full_BSN/Full_BSN.ino
@@ -4,27 +4,26 @@
 
 
 unsigned int net_counter = 0;
-const char* ssid = "PercEvite_P";
+const char* ssid = "PercEvite_Pi";
 const char* password =  "hi64nyvy";
 //IP address to send UDP data to:
 // either use the ip address of the server or 
 // a network broadcast address
-const char * host = "192.168.1.137";
+const char * host = "192.168.4.6";
 const int port = 8000;
 
 //The udp library class
 WiFiUDP udp;
 char incomingPacket[80];  // buffer for incoming packets
-IPAddress local_IP(192, 168, 1, 10);
-IPAddress gateway(192, 168, 1, 1);
+IPAddress local_IP(192, 168, 4, 11);
+IPAddress gateway(192, 168, 4, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 //Broadcast parameters
 uint8_t channel = 0;
 int r;
 esp_err_t esp_wifi_80211_tx(wifi_interface_t ifx, const void *buffer, int len, bool en_sys_seq);
-
-
+unsigned long msg_counter = 0;
 
 // Beacon Packet buffer
 uint8_t packet[] = { 0x80, 0x00, 0x00, 0x00,
@@ -88,35 +87,42 @@ void broadcastSSID(){
   //digitalWrite(21, HIGH);
   random_mac();
   
+  msg_counter++;
+    
+  String msg = "1-"+String(msg_counter);
+  memcpy(&packet[39],msg.c_str(),msg.length());
+ 
   esp_wifi_set_promiscuous(true);
   for(int c = 1;c<15;c++){
     
     packet[80] = c;
     esp_wifi_set_channel(c,WIFI_SECOND_CHAN_NONE);      
-    uint32_t err = esp_wifi_80211_tx(WIFI_IF_AP, packet, 81, true);
+    uint32_t err = esp_wifi_80211_tx(WIFI_IF_STA, packet, 81, true);
        
-    delay(1);  
+    delay(2);  
   }//14 channels
-  delay(10);
+  delay(3);
+  //delay(10);
 
 }
+
 void netCom(){
-  
+   udp.begin(WiFi.localIP(),port);
    //Send a packet
-   for(net_counter = 0;net_counter <50; net_counter++){
+   //for(net_counter = 0;net_counter <2; net_counter++){
     udp.beginPacket(host,port);
     udp.printf("*P-%u-Heap: %u**",net_counter,ESP.getFreeHeap());
     udp.endPacket();
-    delay(50);
-   }
+    delay(100);
+   //}
     udp.flush();
-    
+    udp.stop();
 }
 
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(100);
-  WiFi.mode(WIFI_AP_STA);
+  WiFi.mode(WIFI_STA);
  
   //digitalWrite(21, HIGH); // external antenna on WiPy 3.0
   digitalWrite(21, LOW); // chip antenna on WiPy 3.0
@@ -136,55 +142,52 @@ void setup() {
   packet[39] = 'N';
 }
 
-void loop() {
-  /************Network Communication for Telemetry***********/
-  unsigned long start = micros();
+void wifi_connect(){
   if(WiFi.status() != WL_CONNECTED){
       WiFi.begin(ssid, password);
     }
     while (WiFi.status() != WL_CONNECTED) {
-      delay(5);
+      delay(2);
+      Serial.print('.');
     }
     Serial.println(WiFi.localIP());
-    udp.begin(WiFi.localIP(),port);
-
-    netCom();
- 
     
-    udp.stop();
-    delay(200);
-    WiFi.disconnect();
-    unsigned long stop = micros();
-    Serial.printf("N-dur: %u\r\n",stop-start);
+}
+void loop() {
+    
+    r = random(1,10000);
 
-    //Broadcast and scan
-    for(int i=0;i<10;i++){
-      r = random(1,10000);
-
-  
-      if(r < 6666){
-        for(int k=0;k < 26; k++){
-          packet[39+k] = '*';
-        }
-        unsigned long start = micros();
-        
-        Serial.write(17);//XON
-        Serial.println('>'); //Used to synchronize UART communication
-        
-        int b = Serial.readBytesUntil('*',&packet[39], 26);
-        broadcastSSID();//B   
-        Serial.write(19);//XOFF
-        unsigned long stop = micros();
-        Serial.printf("B-dur: %u\r\n",stop-start);
-        
+    if(r < 5556){
+      //Broadcast
+      for(int k=0;k < 26; k++){
+        packet[39+k] = '*';
       }
-       else{
-        unsigned long start = micros();
-         scan(channel,60);//S
-         unsigned long stop = micros();
-        Serial.printf("S-dur: %u\r\n",stop-start);
-       }
+      
+      unsigned long start = micros();
+      broadcastSSID();//B   
+      unsigned long stop = micros();
+      Serial.printf("B-dur: %u\r\n",stop-start);
+      
     }
+    else if(r > 5555 && r < 7223){
+      //Network
+      unsigned long start = micros();
+      
+      netCom();
+      delay(100);
+      
+      unsigned long stop = micros();
+      Serial.printf("N-dur: %u\r\n",stop-start);
+
+      }
+     else{
+      //Scan
+       unsigned long start = micros();
+       scan(channel,60);//S
+       unsigned long stop = micros();
+       Serial.printf("S-dur: %u\r\n",stop-start);
+     }
+    
   
    
   
